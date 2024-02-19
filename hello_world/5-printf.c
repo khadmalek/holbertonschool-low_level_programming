c0ec7a9b9a64ff988686ad2218fc5ee50429b3da
khadmalek <k.abdelmalek@live.fr>
2024-02-19 16:29:52 +0100

    prints with printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - entry
+*
+* Description 'prints a message'
+*
+* Return: Always 0
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,/n")
+	return 0
+}
