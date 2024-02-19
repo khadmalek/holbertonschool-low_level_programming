6f2957e3907517a299c7ce420d64d42c31960f59
khadmalek <k.abdelmalek@live.fr>
2024-02-19 15:47:23 +0100

    prints sample character

---
+++ hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+*
+* Description 'this function prints a sample message'
+*
+* Return: Alaways 0
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle")
+	return 0
+}
