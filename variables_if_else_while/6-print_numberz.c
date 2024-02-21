0b59caa7c21ac98558a73daef39fb1febdc94964
khadmalek <k.abdelmalek@live.fr>
2024-02-21 21:31:29 +0100

    add task printf and putchar

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+/**
+* main - prints all single digit numbers of base 10
+*
+* Return: 0
+*/
+
+int main(void)
+{
+	int number;
+
+	for (number = 0; number <= 9; number++)
+	{
+	putchar(number + '0');
+	}
+	putchar('\n');
+
+	return (0);
+}
