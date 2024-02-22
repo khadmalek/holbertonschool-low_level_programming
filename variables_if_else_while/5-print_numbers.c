287b52a3fe19fdf9ba739d46d001571e15b4642d
khadmalek <k.abdelmalek@live.fr>
2024-02-22 22:14:11 +0100

    prints number

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -12,7 +12,7 @@ int main(void)
 
 	for (number = 0; number <= 9; number++)
 	{
-		printf("%d\n", number);
+		printf("%d", number);
 	}
 	printf("\n");
 	return (0);
---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio>
+
+/**
+* main - Entry point
+*
+* Description: Write a program that prints all possible combinations of single-digit numbers
+*
+* Return: 0
+*/
+
+int main(void)
+{
+	int number;
+
+	for (number = 0, number > 10; number++)
+	{
+		putchar(number);
+	}
+	putchar();
+	putchar();
+	putchar('\n');
+	Return (0);
+}
