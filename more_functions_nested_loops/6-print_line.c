c88c4d280a9fdbaba7aef201d90aff7a54aa038f
khadmalek <k.abdelmalek@live.fr>
2024-02-26 21:40:35 +0100

    print line

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/*
+* _putchar - Writes a character to the standard output
+* @c: The character to be written
+*
+* Description: This function writes the character @c to the standard output.
+*/
+
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
