cb784c47de632193974607a646ae8b825f6faae0
khadmalek <k.abdelmalek@live.fr>
2024-02-26 15:41:58 +0100

    prints 10 times a function

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+*more_numbers: function that prints 10 times the numbers from to 0 to 14
+*/
+
+void more_numbers(void)
+{
+	int line, digit;
+
+	for (line = 0; line <= 9; line++)
+	{
+		for (digit = 0; digit <= 14; digit++)
+		{
+			if (digit >= 10)
+				_putchar((digit / 10) + '0');
+			_putchar((digit % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
