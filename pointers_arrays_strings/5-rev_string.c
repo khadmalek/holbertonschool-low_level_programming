4bfb5aad4f6bdf2404d823346fbc6dde0e1e65a4
khadmalek <k.abdelmalek@live.fr>
2024-02-28 21:19:40 +0100

    function reverse the order of characters

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+
+/*
+ * rev_string - Reverse a string
+ *
+ * Description: function reverses the order of characters in the given string.
+ * @s: the string to be reversed
+ */
+
+void rev_string(char *s)
+{
+	int length = strlen(s);
+	int i, j;
+	char temp;
+
+	for (i = 0, j = length - 1; i < j; i++, j--)
+	{
+	temp = s[i];
+	s[i] = s[j];
+	s[j] = temp;
+	}
+}
