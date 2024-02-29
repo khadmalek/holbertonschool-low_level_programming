e5a8275bb000edae10cc980e4fc81c331e9c4cd2
khadmalek <k.abdelmalek@live.fr>
2024-02-29 20:58:47 +0100

    fix my code without strlen

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -10,14 +10,19 @@
 
 void rev_string(char *s)
 {
-	int length = strlen(s);
+	int length = 0;
 	int i, j;
 	char temp;
 
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+
 	for (i = 0, j = length - 1; i < j; i++, j--)
 	{
-	temp = s[i];
-	s[i] = s[j];
-	s[j] = temp;
+		temp = s[i];
+		s[i] = s[j];
+		s[j] = temp;
 	}
 }
