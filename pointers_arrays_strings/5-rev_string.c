697be2175cf478ba6ab6be890a1a1113aa5ea3df
khadmalek <k.abdelmalek@live.fr>
2024-03-01 10:38:40 +0100

    fix betty

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,12 +1,14 @@
 #include "main.h"
 #include <string.h>
 
-/*
- * rev_string - Reverse a string
- *
- * Description: function reverses the order of characters in the given string.
- * @s: the string to be reversed
- */
+/**
+*rev_string - Reverse a string
+*
+*@s: thr string to be reversed
+*
+* Description: function reverses the order of characters in the given string.
+* It takes a string as input and modifies it in place to reverse thr order
+*/
 
 void rev_string(char *s)
 {
