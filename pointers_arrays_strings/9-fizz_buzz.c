957f8bd64c492ce07a82e3e726fc49d4f6f66e9f
khadmalek <k.abdelmalek@live.fr>
2024-03-02 09:58:33 +0100

    fizz buzz

---
+++ pointers_arrays_strings/9-fizz_buzz.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description:
+ * This function is the entry point of the program.
+ * It iterates through numbers from 1 to 100,
+ * replacing multiples of 3 with "Fizz", multiples of 5 with "Buzz",
+ * and multiples of both 3 and 5 with "FizzBuzz".
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
