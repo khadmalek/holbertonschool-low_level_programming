d68031b31d328159351e8682a2a050b95f58ef95
khadmalek <k.abdelmalek@live.fr>
2024-03-08 16:44:45 +0100

    function that returns the natural square root of a number

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,45 +1,36 @@
 #include "main.h"
 
 /**
-* _sqrt_recursion - Calcule la racine carrée d'un nbre de manière récursive.
-* @n: Le nombre dont la racine carrée doit être calculée.
-*
-* Description : Cette fonction utilise la récursion pour calculer la racine
-* carrée naturelle d'un nbre donné en appelant la fonction racine.
-* Si le nombre n'est pas un carré parfait, la fonction retourne -1.
-*
-* Return: La racine carrée naturelle de n, -1 si n pas un carré parfait.
-*
-*/
+ *_sqrt_recursion -  func that returns the natural square root of a number.
+ * @n: for number input
+ * Return: 0
+ */
+
 int _sqrt_recursion(int n)
 {
 	if (n == 0)
 		return (0);
+	else if (n == 1)
+		return (1);
 	else if (n < 0)
 		return (-1);
 	else
-		return (racine(n, 1));
+		return (helper(n, 1));
 }
 
 /**
- * racine - calculer la racine carrée d'un nombre.
- * @n: Le nombre dont la racine carrée doit être calculée.
- * @i: L'entier utilisé pour les essais successifs.
- * Return: i or -1
+ * helper - blank
+ * @n: number
+ * @i: incrementor
+ * Return: -1
  */
 
-int racine(int n, int i)
+int helper(int n, int i)
 {
 	if (n == (i * i))
-	{
 		return (i);
-	}
-	else if (i * i > n)
-	{
-		return (-1);
-	}
+	else if (n > (i * i))
+		return (helper(n, i + 1));
 	else
-	{
-		return (racine(n, i + 1));
-	}
+		return (-1);
 }
