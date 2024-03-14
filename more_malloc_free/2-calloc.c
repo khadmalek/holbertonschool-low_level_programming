46fa1475e746dae2aba8383962e172c2dc49e79e
khadmalek <k.abdelmalek@live.fr>
2024-03-14 22:46:37 +0100

    Write a function that allocates memory for an array

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+	unsigned int total_size;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	total_size = nmemb * size;
+	ptr = malloc(total_size);
+
+	if (ptr == NULL)
+	return NULL;
+
+	memset(ptr, 0, total_size);
+
+	return ptr;
+}
