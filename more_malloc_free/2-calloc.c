0f60925a0a805fc6d25429417712b93841d44ead
khadmalek <k.abdelmalek@live.fr>
2024-03-14 22:50:15 +0100

    Write a function that allocates memory for an array

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -2,6 +2,29 @@
 #include <string.h>
 #include "main.h"
 
+#include "main.h"
+
+/**
+* _memset - function fills the first n bytes of the memory area pointed
+*
+* @s: a pointer
+* @b: constant byte
+* @n: number of bytes to fill
+*
+* Return: A pointer to the memory area s
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
+
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *ptr;
