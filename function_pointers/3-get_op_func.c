e9da2670920eb86a0c455db223752b53b1a340b3
khadmalek <k.abdelmalek@live.fr>
2024-03-21 20:49:37 +0100

    fix code

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,30 +1,29 @@
 #include "3-calc.h"
+#include "function_pointers.h"
 
 /**
- * get_op_func - selects the correct function to perform the
- * operation asked by the user
- * @s: operator passed as argument to the program
- * Return: pointer to the function that corresponds to the
- * operator given as a parameter
- */
+ * get_op_func - select the right function depending on given char
+ * @s: given char
+ * Return: selected function
+*/
 int (*get_op_func(char *s))(int, int)
 {
-    op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i = 0;
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
 
-    while (ops[i].op)
-    {
-        if (*(ops[i].op) == *s && *(s + 1) == '\0')
-            return (ops[i].f);
-        i++;
-    }
-    return (NULL);
+	while (ops[i].op != NULL)
+	{
+		if (*s == *ops[i].op)
+		return (ops[i].f);
+		i++;
+	}
+
+	return (NULL);
 }
-
--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -1,56 +1,57 @@
+#include "function_pointers.h"
 #include "3-calc.h"
 
 /**
- * op_add - returns the sum of a and b
- * @a: first integer
- * @b: second integer
- * Return: sum of a and b
- */
+ * op_add - addition of a and b
+ * @a: number 1
+ * @b: number 2
+ * Return: operation result
+*/
 int op_add(int a, int b)
 {
-    return (a + b);
+	return (a + b);
 }
 
 /**
- * op_sub - returns the difference of a and b
- * @a: first integer
- * @b: second integer
- * Return: difference of a and b
- */
+ * op_sub - substraction of a and b
+ * @a: number 1
+ * @b: number 2
+ * Return: operation result
+*/
 int op_sub(int a, int b)
 {
-    return (a - b);
+	return (a - b);
 }
 
 /**
- * op_mul - returns the product of a and b
- * @a: first integer
- * @b: second integer
- * Return: product of a and b
- */
+ * op_mul - multiplication of a and b
+ * @a: number 1
+ * @b: number 2
+ * Return: operation result
+*/
 int op_mul(int a, int b)
 {
-    return (a * b);
+	return (a * b);
 }
 
 /**
- * op_div - returns the result of the division of a by b
- * @a: first integer
- * @b: second integer
- * Return: result of division of a by b
- */
+ * op_div - division of a and b
+ * @a: number 1
+ * @b: number 2
+ * Return: operation result
+*/
 int op_div(int a, int b)
 {
-    return (a / b);
+	return (a / b);
 }
 
 /**
- * op_mod - returns the remainder of the division of a by b
- * @a: first integer
- * @b: second integer
- * Return: remainder of division of a by b
- */
+ * op_mod - modulo of a and b
+ * @a: number 1
+ * @b: number 2
+ * Return: operation result
+*/
 int op_mod(int a, int b)
 {
-    return (a % b);
+	return (a % b);
 }
