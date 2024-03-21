453b79f5f5fdcbf10b2254537a23d33007eb2e04
khadmalek <k.abdelmalek@live.fr>
2024-03-21 18:33:08 +0100

    fix code

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,44 +1,41 @@
-
-#include "3-calc.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
 
 /**
- * main - Prints the result of simple operations.
- * @argc: The number of arguments supplied to the program.
- * @argv: An array of pointers to the arguments.
- *
- * Return: Always 0.
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on success, otherwise 98, 99, or 100
  */
-int main(int __attribute__((__unused__)) argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	int num1, num2;
-	char *op;
+    int a, b;
+    int (*op)(int, int);
 
-	if (argc != 4)
-	{
-		printf("Error\n");
-		exit(98);
-	}
+    if (argc != 4)
+    {
+        printf("Error\n");
+        return (98);
+    }
 
-	num1 = atoi(argv[1]);
-	op = argv[2];
-	num2 = atoi(argv[3]);
+    a = atoi(argv[1]);
+    b = atoi(argv[3]);
+    op = get_op_func(argv[2]);
 
-	if (get_op_func(op) == NULL || op[1] != '\0')
-	{
-		printf("Error\n");
-		exit(99);
-	}
+    if (!op || !argv[2][1])
+    {
+        printf("Error\n");
+        return (99);
+    }
 
-	if ((*op == '/' && num2 == 0) ||
-	    (*op == '%' && num2 == 0))
-	{
-		printf("Error\n");
-		exit(100);
-	}
+    if ((*argv[2] == '/' || *argv[2] == '%') && b == 0)
+    {
+        printf("Error\n");
+        return (100);
+    }
 
-	printf("%d\n", get_op_func(op)(num1, num2));
+    printf("%d\n", op(a, b));
 
-	return (0);
+    return (0);
 }
--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -1,34 +1,56 @@
-#include "calc.h"
+#include "3-calc.h"
 
-int op_add(int a, int b) 
+/**
+ * op_add - returns the sum of a and b
+ * @a: first integer
+ * @b: second integer
+ * Return: sum of a and b
+ */
+int op_add(int a, int b)
 {
     return (a + b);
 }
 
-int op_sub(int a, int b) 
+/**
+ * op_sub - returns the difference of a and b
+ * @a: first integer
+ * @b: second integer
+ * Return: difference of a and b
+ */
+int op_sub(int a, int b)
 {
     return (a - b);
 }
 
-int op_mul(int a, int b) 
+/**
+ * op_mul - returns the product of a and b
+ * @a: first integer
+ * @b: second integer
+ * Return: product of a and b
+ */
+int op_mul(int a, int b)
 {
     return (a * b);
 }
 
-int op_div(int a, int b) 
+/**
+ * op_div - returns the result of the division of a by b
+ * @a: first integer
+ * @b: second integer
+ * Return: result of division of a by b
+ */
+int op_div(int a, int b)
 {
-    if (b == 0) {
-        printf("Error\n");
-        exit (100);
-    }
     return (a / b);
 }
 
-int op_mod(int a, int b) 
+/**
+ * op_mod - returns the remainder of the division of a by b
+ * @a: first integer
+ * @b: second integer
+ * Return: remainder of division of a by b
+ */
+int op_mod(int a, int b)
 {
-    if (b == 0) {
-        printf("Error\n");
-        exit (100);
-    }
     return (a % b);
 }
