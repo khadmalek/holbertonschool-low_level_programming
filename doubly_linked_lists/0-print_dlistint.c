0649ec8cdb4e5b212d571db428f22015f845615b
khadmalek <k.abdelmalek@live.fr>
2024-04-05 10:03:13 +0200

    tast zero

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -2,7 +2,6 @@
 /**
  * print_dlistint - Prints all elements of a dlistint_t list.
  * @h: A pointer to the head of the doubly linked list.
- *
  * Return: The number of nodes in the list.
  */
 size_t print_dlistint(const dlistint_t *h)
